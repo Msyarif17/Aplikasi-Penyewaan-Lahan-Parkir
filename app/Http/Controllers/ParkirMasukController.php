b93d005a40a95a7b76e4d49f220b839958b840a0
NaughtyRhino <m.syarifsetiadi@gmail.com>
2021-12-22 01:31:55 +0700

    add:controller and model

---
+++ app/Http/Controllers/ParkirMasukController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ParkirMasuk;
+use Illuminate\Http\Request;
+
+class ParkirMasukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ParkirMasuk  $parkirMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ParkirMasuk $parkirMasuk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ParkirMasuk  $parkirMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ParkirMasuk $parkirMasuk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ParkirMasuk  $parkirMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ParkirMasuk $parkirMasuk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ParkirMasuk  $parkirMasuk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ParkirMasuk $parkirMasuk)
+    {
+        //
+    }
+}
